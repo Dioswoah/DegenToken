2eecd897e785b3b7cdd1065bdf4bfe0d7f7f08af
Dioswoah <103057088+Dioswoah@users.noreply.github.com>
2024-07-30 20:09:18 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract mod4 is ERC20, Ownable {
+    constructor(uint256 initialAmount, uint8 decimalPlaces, string memory tokenName, string memory tokenSymbol) 
+        ERC20(tokenName, tokenSymbol) 
+        Ownable(msg.sender) 
+    {
+        _mint(msg.sender, initialAmount * (10 ** uint256(decimalPlaces)));
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    // Only the contract owner is allowed to create new tokens
+    function mint(address recipient, uint256 quantity) public onlyOwner {
+        _mint(recipient, quantity);
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    // Any token holder can destroy their own tokens
+    function burn(uint256 quantity) public {
+        _burn(msg.sender, quantity);
+    }
+
+    // Allows token holders to redeem their tokens (burning them)
+    function redeem(uint256 quantity) public {
+        require(balanceOf(msg.sender) >= quantity, "Insufficient token balance");
+        _burn(msg.sender, quantity);
+        // Add any additional logic for redeeming items in the game store here
+    }
+
+    // Overrides the transfer function to enable token transfers
+    function transfer(address receiver, uint256 quantity) public override returns (bool) {
+        return super.transfer(receiver, quantity);
     }
 }
